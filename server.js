6acfd5c6db7332e90ec772e548449cdc7859ab92
Samuel <themainsamuel@gmail.com>
2024-07-12 00:20:25 +0100

    simple localhost server

---
+++ server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// defines the localhost
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
